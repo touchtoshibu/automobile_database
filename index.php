18f5e1628fde1ef2631807daebe78cc93c7ce817
touchtoshibu <65593996+touchtoshibu@users.noreply.github.com>
2021-08-19 12:51:10 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,25 +1,25 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Shibu Kumar - Broken Rock Paper Scissors</title>
+<title>Shibu Kumar - Autos Database</title>
 <?php require_once "bootstrap.php"; ?>
 </head>
 <body>
 <div class="container">
-<h1>Welcome to Broken Rock Paper Scissors</h1>
-<p><strong>Note:</strong> This sample code is only
-partially done and serves only as a starting point for the assignment.
-</p>
+<h1>Welcome to Autos Database</h1>
 <p>
 <a href="login.php">Please Log In</a>
 </p>
 <p>
 Attempt to go to 
-<a href="autos.php">autos.php</a> without logging in - it should fail with an error message.
+<a href="view.php">view.php</a> without logging in - it should fail with an error message.
+</p>
 <p>
-<a href="http://www.wa4e.com/code/rps.zip"
- target="_blank">Source Code for this Application</a>
+Attempt to go to 
+<a href="add.php">add.php</a> without logging in - it should fail with an error message.
+</p>
+<p>
+<a href="https://github.com/touchtoshibu/automobile_database" target="_blank">Source Code of this Application</a>
 </p>
 </div>
 </body>
-
